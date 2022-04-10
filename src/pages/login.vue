34149120129f0dba76a8c102bbced31dd1e7ac08
Michael Vinicius <michaelvini62@gmail.com>
2022-04-10 04:48:57 -0300

    fix: router

--- src/pages/login.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <p>Página de login</p>
-</template>
-<script>
-export default {};
-</script>
-<style  scoped>
-</style>
--- src/pages/profile.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <p>Página de profile</p>
-</template>
-<script>
-export default {};
-</script>
-<style  scoped>
-</style>
